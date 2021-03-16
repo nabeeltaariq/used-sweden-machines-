dc66dcfd61cbc8ecce97a466d06db9a727269613
web8 <web_8@pmsmainoffice.com>
2021-03-16 09:19:04 -0500

    spare parts transparency issues

--- resources/views/templates/public.blade.php
+++ resources/views/templates/public.blade.php
@@ -961,7 +961,7 @@
 
         document.getElementById("nav-logo").style.marginLeft="-100px";
         document.getElementById("nav-logo").style.float="left";
-        document.getElementById("mob-sep").style.marginTop = "-5px";
+        // document.getElementById("mob-sep").style.marginTop = "-5px";
 
 
 
