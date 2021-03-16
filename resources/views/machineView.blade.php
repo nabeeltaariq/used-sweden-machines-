f20a5633b364a672d0de612e68c280c88f6adb67
web8 <web_8@pmsmainoffice.com>
2021-03-16 13:49:23 -0500

    spare parts

--- resources/views/machineView.blade.php
+++ resources/views/machineView.blade.php
@@ -158,6 +158,10 @@
 
         width: 32%;
     }
+    .btn[disabled]
+    {
+        opacity: 0 !important;
+    }
 </style>
 
 <div class="links_navigated_pages" style="font-family:arial;font-size:11px;margin-top:0px;">
@@ -279,6 +283,7 @@
                         </div>
 
                         <button onclick="processRequest(this)" data="partNo={{$part->spare_part_no}}&amp;partTitle={{$part->title}}&amp;price={{$part->price}}&amp;status={{$part->ds}}&amp;manu={{($manufacturer != null ? $manufacturer->title : '')}}" style="display:inline-block;border:1px solid maroon;margin-bottom:5px;margin-left: 15px;padding:5px;background-color:maroon;color:white;height:35px"><span class="fas fa-cart-arrow-down" aria-hidden="true"></span> </button>
+                        <
                     </div>
                 </td>
             </tr>
